9b1d0e0b28d61a1c9a2832cea953c9c067d8839d
TomerADev <tomeraberbach@gmail.com>
2017-09-26 16:14:18 -0400

    Initial commit

---
+++ external/javascript.js
@@ -0,0 +1,35 @@
+function fix() {
+    if (window.innerWidth > 1280) {
+        var nav = document.getElementsByTagName("NAV")[0];
+        if (nav.hasAttribute('class')) {
+            nav.removeAttribute('class');
+        }
+        var lis = nav.getElementsByTagName("LI");
+        for (var i = 0; i < lis.length; i++) {
+            if (lis[i].hasAttribute('class')) {
+                lis[i].removeAttribute('class');
+            }
+        }
+    }
+
+    return window.innerWidth > 1280;
+}
+
+function toggleSections() {
+    if (!fix()) {
+        var nav = document.getElementsByTagName("NAV")[0];
+        if (nav.hasAttribute('class')) {
+            nav.removeAttribute('class');
+        } else {
+            nav.setAttribute('class', 'opennav');
+        }
+        var lis = nav.getElementsByTagName("LI");
+        for (var i = 0; i < lis.length; i++) {
+            if (lis[i].hasAttribute('class')) {
+                lis[i].removeAttribute('class');
+            } else {
+                lis[i].setAttribute('class', 'openli');
+            }
+        }
+    }
+}
